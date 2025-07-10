0c3ae3a34544a501256502b222463949452e97b7
aniqkhan112 <aniqk1122@gmail.com>
2025-07-10 16:13:48 +0500

    update

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -11,6 +11,36 @@ class WebsiteController extends Controller
         return view('pages.home');
     }
 
+    public function contactUs()
+    {
+        return view('pages.contact-us');
+    }
+
+    public function correctionPolicy()
+    {
+        return view('pages.correction-policy');
+    }
+
+    public function faqs()
+    {
+        return view('pages.faqs');
+    }
+
+    public function refundPolicy()
+    {
+        return view('pages.refund-policy');
+    }
+
+    public function reviews()
+    {
+        return view('pages.reviews');
+    }
+
+    public function termsConditions()
+    {
+        return view('pages.terms-conditions');
+    }
+
     public function flightReservation()
     {
         return view('forms.flight-reservation');

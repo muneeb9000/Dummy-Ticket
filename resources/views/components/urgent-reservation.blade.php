6b263a165b7974f7823ba8d64a7827e8460a9fae
Hafiz Muneeb <championsays786@gmail.com>
2025-07-17 16:55:41 +0500

    update the code

--- resources/views/components/urgent-reservation.blade.php
+++ resources/views/components/urgent-reservation.blade.php
@@ -1,30 +1,44 @@
-<div>
+<div x-data="{
+    urgentAmount: 0,
+    urgentLabel: '',
+    handleUrgentChange(event) {
+        const value = event.target.value;
+        if (value === '1') {
+            this.urgentAmount = 30;
+            this.urgentLabel = '6 Hours';
+        } else if (value === '2') {
+            this.urgentAmount = 20;
+            this.urgentLabel = '8 Hours';
+        } else {
+            this.urgentAmount = 0;
+            this.urgentLabel = '';
+        }
+        this.$dispatch('update-urgent-summary', {
+            urgentAmount: this.urgentAmount,
+            urgentLabel: this.urgentLabel
+        });
+    }
+}">
     <div class="mx-auto p-4">
-                                    <!-- Heading -->
-                                    <p class="mt-6 text-secondary font-semibold text-[16px]">
-                                        Do You Want Your Reservation URGENT?
-                                    </p>
-                                    <p class="text-[14px] text-success">
-                                        Typically URGENT Email Delivery Time Period is 6-8 Hours!
-                                    </p>
+        <!-- Heading -->
+        <p class="mt-6 text-secondary font-semibold text-[16px]">
+            Do You Want Your Reservation URGENT?
+        </p>
+        <p class="text-[14px] text-success">
+            Typically URGENT Email Delivery Time Period is 6-8 Hours!
+        </p>
 
-                                    <!-- Visa Purpose Dropdown -->
-                                    <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mt-4 text-sm items-start">
-                                        <div class="col-span-12 md:col-span-6">
-                                            <label
-                                                class="block mb-1 font-quicksand text-sm font-medium text-tealDeep mb-2">
-                                                For which visa purpose is this urgent reservation? *
-                                            </label>
-                                            <select name="visa_purpose" class="form-input w-full">
-                                                <option value="" disabled selected>Select a purpose</option>
-                                                <option value="Visitor_Visa">Visitor Visa</option>
-                                                <option value="Student_Visa">Student Visa</option>
-                                                <option value="Work_Visa">Work Visa</option>
-                                                <option value="Immigration_Visa">Immigration Visa</option>
-                                            </select>
-                                            <span x-show="errors.visa_purpose" x-text="errors.visa_purpose"
-                                                class="text-success text-xs mt-1"></span>
-                                        </div>
-                                    </div>
-                                </div>
+        <!-- Urgent Reservation Dropdown -->
+        <div class="grid grid-cols-1 md:grid-cols-12 gap-4 mt-4 text-sm items-start">
+            <div class="col-span-12 md:col-span-6">
+                <select name="urgent_reservation" class="form-input w-full" @change="handleUrgentChange($event)">
+                    <option value="" selected>Select Please</option>
+                    <option value="1">6 Hours ($30 Extra for urgent)</option>
+                    <option value="2">8 Hours ($20 Extra for urgent)</option>
+                </select>
+                <span x-show="errors.urgent_reservation" x-text="errors.urgent_reservation"
+                    class="text-success text-xs mt-1"></span>
+            </div>
+        </div>
+    </div>
 </div>
